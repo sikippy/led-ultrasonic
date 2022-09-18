495f15d28f909a1405a2ea1010a287d7afbb6d8b
Vina <28854236+sikippy@users.noreply.github.com>
2022-09-18 02:16:20 +0200

    add led to display the distance, define pin amount

--- code.c
+++ code.c
@@ -1,6 +1,11 @@
+#include <LiquidCrystal.h>
+
 #define ECHO_PIN 2
 #define TRIG_PIN 15
+#define pinAmount 4
 #define NULL ((void *)0)
+
+LiquidCrystal lcd(12, 13, 14, 27, 26, 25);
 
 struct Node{
   int index;
@@ -41,22 +46,35 @@ float readDistance(){
 }
 
 void setup() {
-  for (int i=0;i<4;i++){
+  for (int i=0;i<pinAmount;i++){
   insertArray(i,arrayLed[i][0],arrayLed[i][1]);
   }
   Serial.begin(115200);
+  lcd.begin(16, 2);
+  lcd.clear();
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+}
+
+void display(float distance, int ledamount){ 
+    lcd.clear();
+    lcd.print("distance = "); 
+    lcd.print(distance,5);
+    lcd.setCursor(0, 1);
+    ledamount>0?lcd.print("led on "):lcd.print("led off ");
 }
 
 void loop() {
    float distance = readDistance();
    struct Node* ptr;
    ptr = head;
-
+   
    while (ptr != NULL) { 
       int idx = distance>ptr->distance?ptr->index:0;
-      Serial.println(idx);
+      display(distance,idx);
+      if (idx==pinAmount){
+        setHighLow(ECHO_PIN,10,HIGH);
+      }
       for(int j=0; j<idx;j++){
        setHighLow(ptr->pin,100,HIGH);
       }
